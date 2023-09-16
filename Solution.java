5eaedc48749f8c44ea481041ed92658fba983cc3
Suhas Bharadwaj K <tak.kumara@gmail.com>
2023-09-16 23:10:42 +0530

    Update Solution.java

--- Solution.java
+++ Solution.java
@@ -1,112 +1,32 @@
-class Solution 
-{
-    static int[][] isPacificOceanReachable;
-    static int[][] isAtlanticOceanReachable;
-    
-    public static boolean canReachPacificOcean(int[][] heights, int i, int j)
-    {
-        if(isPacificOceanReachable[i][j] != -1)
-        {
-            return (isPacificOceanReachable[i][j] == 1);
-        }
-        else if(i != 0 ? heights[i-1][j] <= heights[i][j] : false)
-        {
-            isPacificOceanReachable[i][j] = canReachPacificOcean(heights, i-1, j) == true? 1 : 0;
-        }
-        else if(i != heights.length - 1 ? heights[i+1][j] <= heights[i][j] : false)
-        {
-            isPacificOceanReachable[i][j] = canReachPacificOcean(heights, i+1, j) == true ? 1 : 0;
-        }
-        else if(j != 0 ? heights[i][j-1] <= heights[i][j] : false)
-        {
-            isPacificOceanReachable[i][j] = canReachPacificOcean(heights, i, j-1) == true ? 1 : 0;
-        }
-        else if(j != heights[0].length - 1 ? heights[i][j+1] <= heights[i][j] : false)
-        {
-            isPacificOceanReachable[i][j] = canReachPacificOcean(heights, i, j+1) == true ? 1 : 0;
-        }
-        else
-        {
-            isPacificOceanReachable[i][j] = 0;
-        }
-        
-        return  (isPacificOceanReachable[i][j] == 1);
-    }
-    
-    public static boolean canReachAtlanticOcean(int[][] heights, int i, int j)
-    {
-        System.out.println("i = " + i + "\t" + "j = " + j);
-        
-        if(isAtlanticOceanReachable[i][j] != -1)
-        {
-            return (isAtlanticOceanReachable[i][j] == 1);
-        }
-        else if(i != 0 ? heights[i-1][j] <= heights[i][j] : false)
-        {
-            isAtlanticOceanReachable[i][j] = canReachAtlanticOcean(heights, i-1, j) == true ? 1 : 0;
-        }
-        else if(i != heights.length - 1 ? heights[i+1][j] <= heights[i][j] : false)
-        {
-            isAtlanticOceanReachable[i][j] = canReachAtlanticOcean(heights, i+1, j) == true ? 1 : 0;
-        }
-        else if(j != 0 ? heights[i][j-1] <= heights[i][j] : false)
-        {
-            isAtlanticOceanReachable[i][j] = canReachAtlanticOcean(heights, i, j-1) == true ? 1 : 0;
-        }
-        else if(j != heights[0].length - 1 ? heights[i][j+1] <= heights[i][j] : false)
-        {
-            isAtlanticOceanReachable[i][j] = canReachAtlanticOcean(heights, i, j+1) == true ? 1 : 0;
-        }
-        else
-        {
-            isAtlanticOceanReachable[i][j] = 0;
-        }
-        return (isAtlanticOceanReachable[i][j] == 1);
-    } 
-    
-    public List<List<Integer>> pacificAtlantic(int[][] heights) 
-    {
-        isPacificOceanReachable = new int[heights.length][heights[0].length];
-        isAtlanticOceanReachable = new int[heights.length][heights[0].length];
-        
-        for(int i=0;i<heights.length;i++)
-        {
-            for(int j=0;j<heights[0].length;j++)
-            {
-                if(i == 0 || j == 0) 
-                {
-                    isPacificOceanReachable[i][j] = 1; 
-                    isAtlanticOceanReachable[i][j] = -1;
-                }
-                else if(i == heights.length - 1 || j == heights[0].length - 1)
-                {
-                    isAtlanticOceanReachable[i][j] = 1;
-                    isPacificOceanReachable[i][j] = -1;
-                }
-                else
-                {
-                    isPacificOceanReachable[i][j] = -1;
-                    isAtlanticOceanReachable[i][j] = -1;
+class Solution {
+    public int minimumEffortPath(int[][] heights) {
+        int m = heights.length, n= heights[0].length;
+        int[][] result = new int[m][n];
+        for(int[] r: result)
+            Arrays.fill(r, Integer.MAX_VALUE);
+        PriorityQueue<int[]> minHeap= new PriorityQueue<int[]>((a,b)->a[2]-b[2]);
+        int[][] dirs=new int[][]{{0,1},{1,0},{0,-1},{-1,0}};
+        int minEffort = 0;
+        minHeap.offer(new int[]{0,0,0});
+        result[0][0]=0;
+        while(!minHeap.isEmpty()){
+            int[] node =minHeap.poll();
+            int x =node[0], y=node[1], effort=node[2];
+            if (x==m-1 && y==n-1)
+                return effort;
+            for(int[] dir:dirs){
+                int newX= x+dir[0];
+                int newY= y+dir[1];
+                if (newX>=0 && newX <m && newY >=0 && newY <n){
+                    int absDiff = Math.abs(heights[newX][newY]-heights[x][y]);
+                    int threshold = Math.max(effort, absDiff);
+                    if (threshold< result[newX][newY]){
+                        result[newX][newY]=threshold;
+                        minHeap.offer(new int[]{newX,newY, threshold});
+                    }
                 }
             }
         }
-        
-        List<List<Integer>> goodCoordinates = new ArrayList<List<Integer>>();
-        
-        for(int i=0;i<heights.length;i++)
-        {
-            for(int j=0;j<heights[0].length;j++)
-            {
-                if(canReachPacificOcean(heights, i, j) && canReachAtlanticOcean(heights, i, j))
-                {
-                    List<Integer> coordinate = new ArrayList<Integer>();
-                    coordinate.add(i);
-                    coordinate.add(j);
-                    
-                    goodCoordinates.add(coordinate);
-                }
-            }
-        }
-        return goodCoordinates;   
+        return result[m-1][n-1];
     }
 }
